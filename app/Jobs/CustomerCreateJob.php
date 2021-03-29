cad99c89780b52de104222cffd3b8af303bd9bf1
haseebtanveer1999 <haseeb@tetralogicx.com>
2021-03-29 15:06:46 +0500

    Webhook for New Customer Welcome sms

--- app/Jobs/CustomerCreateJob.php
+++ app/Jobs/CustomerCreateJob.php
@@ -1,13 +1,18 @@
 <?php namespace App\Jobs;
 
+use App\Address;
+use App\Customer;
+use App\Http\Controllers\LogsController;
 use App\Test;
 use App\User;
 use App\Welcomecampaign;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
 use Osiset\ShopifyApp\Objects\Values\ShopDomain;
 use stdClass;
 
@@ -28,6 +33,7 @@ class CustomerCreateJob implements ShouldQueue
      * @var object
      */
     public $data;
+    public $log_store;
 
     /**
      * Create a new job instance.
@@ -41,6 +47,7 @@ class CustomerCreateJob implements ShouldQueue
     {
         $this->shopDomain = $shopDomain;
         $this->data = $data;
+        $this->log_store = new LogsController();
     }
 
     /**
@@ -50,21 +57,83 @@ class CustomerCreateJob implements ShouldQueue
      */
     public function handle()
     {
-        try {
+        $pushed_users=[];
 
-            $custom_shop = $this->shopDomain;
-            $test = new Test();
-            $test->text = "shopdomain #:" .$custom_shop;
-            $test->save();
-            $shop = User::where('name', $custom_shop)->first();
-            $custom_data=$this->data;
+        $custom_shop = $this->shopDomain;
+        $shop = User::where('name', $custom_shop)->first();
+        $customer_data=$this->data;
 
+        foreach ($this->data as $customer_check){
+            $customer = Customer::where('user_id', $shop->id)->where('shopify_customer_id', $customer_check->id)->first();
+            if($customer === null){
+                $customer = New Customer();
+            }
 
+            $customer->shopify_customer_id = $customer_check->id;
+            $customer->user_id = $shop->id;
 
-            $welcome_campaign = Welcomecampaign::where('user_id', $shop->id)->first();
+            $customer->email = $customer_check->email;
+            $customer->first_name = $customer_check->first_name;
+            $customer->last_name = $customer_check->last_name;
+            $customer->phone = $customer_check->phone;
+            $customer->currency = $customer_check->currency;
+            $customer->accepts_marketing = $customer_check->accepts_marketing;
+            $customer->state = $customer_check->state;
+            $customer->note = $customer_check->note;
+            $customer->orders_count = $customer_check->orders_count;
+            $customer->total_spent = $customer_check->total_spent;
+            $customer->last_order_id = $customer_check->last_order_id;
+            $customer->addresses = json_encode($customer_check->addresses);
+            $customer->verified_email = $customer_check->verified_email;
+            $customer->accepts_marketing_updated_at = Carbon::createFromTimeString($customer_check->accepts_marketing_updated_at)->format('Y-m-d H:i:s');
+            $customer->marketing_opt_in_level = $customer_check->marketing_opt_in_level;
+            $customer->save();
+
+            foreach($customer_check->addresses as $address){
+                $address_customer = Address::where('shopify_customer_id', $address->customer_id)->where('shopify_address_id', $address->id)->first();
+                if($address_customer === null){
+                    $address_customer = new Address();
+                }
+                $address_customer->shopify_address_id = $address->id;
+                $address_customer->shopify_customer_id = $address->customer_id;
+//            $address_customer->shopify_shop_id = Auth::user()->id;
+                $address_customer->first_name = $address->first_name;
+                $address_customer->last_name = $address->last_name;
+                $address_customer->company = $address->company;
+                $address_customer->address1 = $address->address1;
+                $address_customer->address2 = $address->address2;
+                $address_customer->city = $address->city;
+                $address_customer->province = $address->province;
+                $address_customer->country = $address->country;
+                $address_customer->zip = $address->zip;
+                $address_customer->phone = $address->phone;
+                $address_customer->name = $address->name;
+                $address_customer->province_code = $address->province_code;
+                $address_customer->country_code = $address->country_code;
+                $address_customer->default = $address->default;
+                $address_customer->save();
+            }
+
+        }
+
+//            save customer end
+
+            $user_select_countries = $shop->countries;
+            foreach ($user_select_countries as $countries) {
+                foreach ($customer->addressess as $add){
+                    if($add->country == $countries->name){
+                        $users = json_decode(json_encode($customer, TRUE));
+                        array_push($pushed_users , $users);
+                    }
+                }
+
+            }
+
+        foreach ($pushed_users as $user_data){
+            $welcome_campaign = Welcomecampaign::where('user_id', $user_data->id)->first();
             $data = [
                 "from" => $welcome_campaign->sender_name,
-                "to" => $custom_data->phone,
+                "to" => $customer->phone,
                 "text" => $welcome_campaign->message_text,
             ];
 
@@ -96,13 +165,16 @@ class CustomerCreateJob implements ShouldQueue
                 $test = new Test();
                 $test->number = 404;
                 $test->text = "cURL Error #:" .$err;
+                $this->log_store->log_store( $user_data->id, 'Welcomecampaign', $welcome_campaign->id, $welcome_campaign->campaign_name, 'Welcome Sms not Sended' , $notes = $err);
+        //
                 $test->save();
             } else {
                 $test = new Test();
                 $test->number = 200;
                 $test->text = "Successful Staus:" .$response;
                 $test->save();
-//                Detect Credits
+                $this->log_store->log_store( $user_data->id, 'Welcomecampaign', $welcome_campaign->id, $welcome_campaign->campaign_name, 'Welcome Sms Sended Successfully to new Customer' , $notes = $response);
+        //                Detect Credits
                 $user = User::Where('id', $welcome_campaign->user_id)->first();
                 if($user->credit >= 0){
                     $user->credit =  $user->credit - 1;
@@ -111,18 +183,9 @@ class CustomerCreateJob implements ShouldQueue
                 }
                 $user->save();
             }
-        } catch (\Exception $exception){
-            $new = new Test();
-            $new->text = "catch error:".$exception->getMessage();
-            $new->save();
         }
         // Convert domain
-
-
-//        $this->customer->welcome_sms_status = $this->welcome_sms_status;
-//        $this->customer->save();
-
-        // Do what you wish with the data
-        // Access domain name as $this->shopDomain->toNative()
     }
+
+
 }
