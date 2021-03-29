cafa2265a6aee406c131907341ea935a5de4613d
haseebtanveer1999 <haseeb@tetralogicx.com>
2021-03-29 15:18:16 +0500

    Webhook for New Customer Welcome sms

--- app/Jobs/CustomerCreateJob.php
+++ app/Jobs/CustomerCreateJob.php
@@ -13,7 +13,9 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Auth;
+use Mockery\Exception;
 use Osiset\ShopifyApp\Objects\Values\ShopDomain;
+use PhpParser\Node\Stmt\TryCatch;
 use stdClass;
 
 class CustomerCreateJob implements ShouldQueue
@@ -58,38 +60,36 @@ class CustomerCreateJob implements ShouldQueue
     public function handle()
     {
         $pushed_users=[];
-
         $custom_shop = $this->shopDomain;
         $shop = User::where('name', $custom_shop)->first();
         $customer_data=$this->data;
-
-        foreach ($this->data as $customer_check){
-            $customer = Customer::where('user_id', $shop->id)->where('shopify_customer_id', $customer_check->id)->first();
+        try {
+            $customer = Customer::where('user_id', $shop->id)->where('shopify_customer_id', $customer_data->id)->first();
             if($customer === null){
                 $customer = New Customer();
             }
 
-            $customer->shopify_customer_id = $customer_check->id;
+            $customer->shopify_customer_id = $customer_data->id;
             $customer->user_id = $shop->id;
 
-            $customer->email = $customer_check->email;
-            $customer->first_name = $customer_check->first_name;
-            $customer->last_name = $customer_check->last_name;
-            $customer->phone = $customer_check->phone;
-            $customer->currency = $customer_check->currency;
-            $customer->accepts_marketing = $customer_check->accepts_marketing;
-            $customer->state = $customer_check->state;
-            $customer->note = $customer_check->note;
-            $customer->orders_count = $customer_check->orders_count;
-            $customer->total_spent = $customer_check->total_spent;
-            $customer->last_order_id = $customer_check->last_order_id;
-            $customer->addresses = json_encode($customer_check->addresses);
-            $customer->verified_email = $customer_check->verified_email;
-            $customer->accepts_marketing_updated_at = Carbon::createFromTimeString($customer_check->accepts_marketing_updated_at)->format('Y-m-d H:i:s');
-            $customer->marketing_opt_in_level = $customer_check->marketing_opt_in_level;
+            $customer->email = $customer_data->email;
+            $customer->first_name = $customer_data->first_name;
+            $customer->last_name = $customer_data->last_name;
+            $customer->phone = $customer_data->phone;
+            $customer->currency = $customer_data->currency;
+            $customer->accepts_marketing = $customer_data->accepts_marketing;
+            $customer->state = $customer_data->state;
+            $customer->note = $customer_data->note;
+            $customer->orders_count = $customer_data->orders_count;
+            $customer->total_spent = $customer_data->total_spent;
+            $customer->last_order_id = $customer_data->last_order_id;
+            $customer->addresses = json_encode($customer_data->addresses);
+            $customer->verified_email = $customer_data->verified_email;
+            $customer->accepts_marketing_updated_at = Carbon::createFromTimeString($customer_data->accepts_marketing_updated_at)->format('Y-m-d H:i:s');
+            $customer->marketing_opt_in_level = $customer_data->marketing_opt_in_level;
             $customer->save();
 
-            foreach($customer_check->addresses as $address){
+            foreach($customer_data->addresses as $address){
                 $address_customer = Address::where('shopify_customer_id', $address->customer_id)->where('shopify_address_id', $address->id)->first();
                 if($address_customer === null){
                     $address_customer = new Address();
@@ -114,76 +114,83 @@ class CustomerCreateJob implements ShouldQueue
                 $address_customer->save();
             }
 
+        }catch (\Exception $exception){
+            $new = new Test();
+            $new->text = "error: ".$exception->getMessage();
+            $new->save();
         }
+
+
+
 
 //            save customer end
 
-            $user_select_countries = $shop->countries;
-            foreach ($user_select_countries as $countries) {
-                foreach ($customer->addressess as $add){
-                    if($add->country == $countries->name){
-                        $users = json_decode(json_encode($customer, TRUE));
-                        array_push($pushed_users , $users);
-                    }
-                }
-
-            }
-
-        foreach ($pushed_users as $user_data){
-            $welcome_campaign = Welcomecampaign::where('user_id', $user_data->id)->first();
-            $data = [
-                "from" => $welcome_campaign->sender_name,
-                "to" => $customer->phone,
-                "text" => $welcome_campaign->message_text,
-            ];
-
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => "http://api.messaging-service.com/sms/1/text/single",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => $data,
-                CURLOPT_HTTPHEADER => array(
-                    "accept: application/json",
-                    "authorization: Basic c2hvcGlmeWFwcC50ZXh0Z2xvYmFsOlRHc2hvcGlmeTEh",
-                    "cache-control: no-cache",
-                    "content-type: application/json",
-                    "postman-token: 04d5825f-6285-666b-6d0c-968ce3f6fd25"
-                ),
-            ));
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            curl_close($curl);
-
-            if ($err) {
-                $test = new Test();
-                $test->number = 404;
-                $test->text = "cURL Error #:" .$err;
-                $this->log_store->log_store( $user_data->id, 'Welcomecampaign', $welcome_campaign->id, $welcome_campaign->campaign_name, 'Welcome Sms not Sended' , $notes = $err);
-        //
-                $test->save();
-            } else {
-                $test = new Test();
-                $test->number = 200;
-                $test->text = "Successful Staus:" .$response;
-                $test->save();
-                $this->log_store->log_store( $user_data->id, 'Welcomecampaign', $welcome_campaign->id, $welcome_campaign->campaign_name, 'Welcome Sms Sended Successfully to new Customer' , $notes = $response);
-        //                Detect Credits
-                $user = User::Where('id', $welcome_campaign->user_id)->first();
-                if($user->credit >= 0){
-                    $user->credit =  $user->credit - 1;
-                }else{
-                    $user->credit_status = "0 credits";
-                }
-                $user->save();
-            }
-        }
+//            $user_select_countries = $shop->countries;
+//            foreach ($user_select_countries as $countries) {
+//                foreach ($customer->addressess as $add){
+//                    if($add->country == $countries->name){
+//                        $users = json_decode(json_encode($customer, TRUE));
+//                        array_push($pushed_users , $users);
+//                    }
+//                }
+//
+//            }
+//
+//            foreach ($pushed_users as $user_data){
+//            $welcome_campaign = Welcomecampaign::where('user_id', $user_data->id)->first();
+//            $data = [
+//                "from" => $welcome_campaign->sender_name,
+//                "to" => $customer->phone,
+//                "text" => $welcome_campaign->message_text,
+//            ];
+//
+//            $curl = curl_init();
+//            curl_setopt_array($curl, array(
+//                CURLOPT_URL => "http://api.messaging-service.com/sms/1/text/single",
+//                CURLOPT_RETURNTRANSFER => true,
+//                CURLOPT_ENCODING => "",
+//                CURLOPT_MAXREDIRS => 10,
+//                CURLOPT_TIMEOUT => 30,
+//                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//                CURLOPT_CUSTOMREQUEST => "POST",
+//                CURLOPT_POSTFIELDS => $data,
+//                CURLOPT_HTTPHEADER => array(
+//                    "accept: application/json",
+//                    "authorization: Basic c2hvcGlmeWFwcC50ZXh0Z2xvYmFsOlRHc2hvcGlmeTEh",
+//                    "cache-control: no-cache",
+//                    "content-type: application/json",
+//                    "postman-token: 04d5825f-6285-666b-6d0c-968ce3f6fd25"
+//                ),
+//            ));
+//
+//            $response = curl_exec($curl);
+//            $err = curl_error($curl);
+//
+//            curl_close($curl);
+//
+//            if ($err) {
+//                $test = new Test();
+//                $test->number = 404;
+//                $test->text = "cURL Error #:" .$err;
+//                $this->log_store->log_store( $user_data->id, 'Welcomecampaign', $welcome_campaign->id, $welcome_campaign->campaign_name, 'Welcome Sms not Sended' , $notes = $err);
+//        //
+//                $test->save();
+//            } else {
+//                $test = new Test();
+//                $test->number = 200;
+//                $test->text = "Successful Staus:" .$response;
+//                $test->save();
+//                $this->log_store->log_store( $user_data->id, 'Welcomecampaign', $welcome_campaign->id, $welcome_campaign->campaign_name, 'Welcome Sms Sended Successfully to new Customer' , $notes = $response);
+//        //                Detect Credits
+//                $user = User::Where('id', $welcome_campaign->user_id)->first();
+//                if($user->credit >= 0){
+//                    $user->credit =  $user->credit - 1;
+//                }else{
+//                    $user->credit_status = "0 credits";
+//                }
+//                $user->save();
+//            }
+//        }
         // Convert domain
     }
 
