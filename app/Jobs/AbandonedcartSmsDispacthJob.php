b721051ea3b55a883baa156b345cdee8f1e2dd2f
haseebtanveer1999 <haseeb@tetralogicx.com>
2021-03-31 20:26:12 +0500

    order update, create, checkout, webhook

--- app/Jobs/AbandonedcartSmsDispacthJob.php
+++ app/Jobs/AbandonedcartSmsDispacthJob.php
@@ -38,82 +38,89 @@ class AbandonedcartSmsDispacthJob implements ShouldQueue
      */
     public function handle()
     {
-        $checkout = $this->checkout;
-        $shop = $this->shop;
-        $customer_id = $checkout->customer->id;
-        $customer = $shop->api()->rest('GET', '/admin/customers/'.$customer_id.'.json')['body']['customer'];
-        $customer_country = $customer->addresses['0']->country;
-        $customer_phone = $customer->addresses['0']->phone;
+        try {
+            $checkout = $this->checkout;
+            $shop = $this->shop;
+            $customer_id = $checkout->customer->id;
+            $customer = $shop->api()->rest('GET', '/admin/customers/'.$customer_id.'.json')['body']['customer'];
+            $customer_country = $customer->addresses['0']->country;
+            $customer_phone = $customer->addresses['0']->phone;
 
-        $country_users = $shop->countries;
-//                dd($country_users);
-        foreach ($country_users as $country_user){
-            if($country_user->name == $customer_country){
-                $abandoned_cart_campaign = Abandonedcartcampaign::where('user_id', $shop->id)->first();
+            $country_users = $shop->countries;
+    //                dd($country_users);
+            foreach ($country_users as $country_user){
+                if($country_user->name == $customer_country){
+                    $abandoned_cart_campaign = Abandonedcartcampaign::where('user_id', $shop->id)->first();
 
-                $messgae_text = str_replace('{CustomerName}',$checkout->customer->first_name." ".$checkout->customer->last_name,$abandoned_cart_campaign->message_text);
-                $test = new Test();
-                $test->text = "Text Message customer name Variable:" .$messgae_text;
-                $test->save();
-                $data = [
-                    "from" => $abandoned_cart_campaign->sender_name,
-                    "to" => $customer_phone,
-                    "text" => $messgae_text,
-                ];
-
-                $curl = curl_init();
-                curl_setopt_array($curl, array(
-                    CURLOPT_URL => "http://api.messaging-service.com/sms/1/text/single",
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => "",
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 30,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => "POST",
-                    CURLOPT_POSTFIELDS => json_encode($data),
-                    CURLOPT_HTTPHEADER => array(
-                        "accept: application/json",
-                        "authorization: Basic c2hvcGlmeWFwcC50ZXh0Z2xvYmFsOlRHc2hvcGlmeTEh",
-                        "cache-control: no-cache",
-                        "content-type: application/json",
-                        "postman-token: 04d5825f-6285-666b-6d0c-968ce3f6fd25"
-                    ),
-                ));
-
-                $response = curl_exec($curl);
-                $err = curl_error($curl);
-
-                curl_close($curl);
-
-                if ($err) {
+                    $messgae_text = str_replace('{CustomerName}',$checkout->customer->first_name." ".$checkout->customer->last_name,$abandoned_cart_campaign->message_text);
                     $test = new Test();
-                    $test->number = 404;
-                    $test->text = "cURL Error #:" .$err;
-                    $this->log_store->log_store( $shop->id, 'Abandonedcartcampaign', $abandoned_cart_campaign->id, $abandoned_cart_campaign->campaign_name, 'Abandoned Cart SMS not Sended');
-                    //
+                    $test->text = "Text Message customer name Variable:" .$messgae_text;
                     $test->save();
-                } else {
-                    $test = new Test();
-                    $test->number = 200;
-                    $test->text = "Successful Staus:" .$response;
-                    $test->save();
-                    $this->log_store->log_store($shop->id, 'Abandonedcartcampaign', $abandoned_cart_campaign->id, $abandoned_cart_campaign->campaign_name, 'Abandoned Cart SMS Sended Successfully to Customer ('.$checkout->customer->first_name.')');
-                    //                Detect Credits
-                    $user = User::Where('id', $abandoned_cart_campaign->user_id)->first();
-                    if($user->credit >= 0){
-                        $user->credit =  $user->credit - 1;
-                    }else{
-                        $user->credit_status = "0 credits";
+                    $data = [
+                        "from" => $abandoned_cart_campaign->sender_name,
+                        "to" => $customer_phone,
+                        "text" => $messgae_text,
+                    ];
+
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => "http://api.messaging-service.com/sms/1/text/single",
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => "",
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 30,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => "POST",
+                        CURLOPT_POSTFIELDS => json_encode($data),
+                        CURLOPT_HTTPHEADER => array(
+                            "accept: application/json",
+                            "authorization: Basic c2hvcGlmeWFwcC50ZXh0Z2xvYmFsOlRHc2hvcGlmeTEh",
+                            "cache-control: no-cache",
+                            "content-type: application/json",
+                            "postman-token: 04d5825f-6285-666b-6d0c-968ce3f6fd25"
+                        ),
+                    ));
+
+                    $response = curl_exec($curl);
+                    $err = curl_error($curl);
+
+                    curl_close($curl);
+
+                    if ($err) {
+                        $test = new Test();
+                        $test->number = 404;
+                        $test->text = "cURL Error #:" .$err;
+                        $this->log_store->log_store( $shop->id, 'Abandonedcartcampaign', $abandoned_cart_campaign->id, $abandoned_cart_campaign->campaign_name, 'Abandoned Cart SMS not Sended');
+                        //
+                        $test->save();
+                    } else {
+                        $test = new Test();
+                        $test->number = 200;
+                        $test->text = "Successful Staus:" .$response;
+                        $test->save();
+                        $this->log_store->log_store($shop->id, 'Abandonedcartcampaign', $abandoned_cart_campaign->id, $abandoned_cart_campaign->campaign_name, 'Abandoned Cart SMS Sended Successfully to Customer ('.$checkout->customer->first_name.')');
+                        //                Detect Credits
+                        $user = User::Where('id', $abandoned_cart_campaign->user_id)->first();
+                        if($user->credit >= 0){
+                            $user->credit =  $user->credit - 1;
+                        }else{
+                            $user->credit_status = "0 credits";
+                        }
+                        $user->save();
                     }
-                    $user->save();
-                }
 
+                }
             }
+
+            $abandoned_cart = new AbandonedCartLog();
+            $abandoned_cart->checkout_id = $checkout->id;
+            $abandoned_cart->user_id = $shop->id;
+            $abandoned_cart->save();
+        }catch (\Exception $exception){
+            $new = new Test();
+            $new->text = "error: ".$exception->getMessage();
+            $new->save();
         }
 
-        $abandoned_cart = new AbandonedCartLog();
-        $abandoned_cart->checkout_id = $checkout->id;
-        $abandoned_cart->user_id = Auth::user()->id;
-        $abandoned_cart->save();
     }
 }
