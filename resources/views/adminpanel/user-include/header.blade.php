9cfef54d790f74e8a37462c0846536176c5abf35
haseebtanveer1999 <haseeb@tetralogicx.com>
2021-07-17 12:18:53 +0500

    maitnain new logs

--- resources/views/adminpanel/user-include/header.blade.php
+++ resources/views/adminpanel/user-include/header.blade.php
@@ -19,11 +19,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('/custom_assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('/custom_assets/css/style.css')}}?{{ now() }}">
 {{--    datepicker css--}}
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     <!-- <link rel="stylesheet" href="http://localhost:3000/css/bootstrap4/dist/css/bootstrap-custom.css?v=datetime"> -->
-    <link rel="stylesheet" href="{{asset('polished_asset/polished.min.css')}}">
+    <link rel="stylesheet" href="{{asset('polished_asset/polished.min.css')}}?{{ now() }}">
     <!-- <link rel="stylesheet" href="polaris-navbar.css"> -->
     <link rel="stylesheet" href="{{asset('polished_asset/iconic/css/open-iconic-bootstrap.min.css')}}">
 {{--select2 css--}}
