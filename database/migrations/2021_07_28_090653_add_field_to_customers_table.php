36a199710ba3f3dc7e520ddeba1a3dcc59cb41d1
haseebtanveer1999 <haseeb@tetralogicx.com>
2021-07-28 15:44:07 +0500

    lapsed customer campaign

--- database/migrations/2021_07_28_090653_add_field_to_customers_table.php
+++ database/migrations/2021_07_28_090653_add_field_to_customers_table.php
@@ -14,7 +14,7 @@ class AddFieldToCustomersTable extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->timestamp('last_order_date')->nullable()->after('last_name');
+            $table->string('last_order_date')->nullable()->after('last_name');
         });
     }
 
