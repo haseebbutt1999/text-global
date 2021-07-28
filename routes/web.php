0496411dec48d12523b46954f5e153f5ef635062
haseebtanveer1999 <haseeb@tetralogicx.com>
2021-07-28 15:51:10 +0500

    lapsed customer campaign

--- routes/web.php
+++ routes/web.php
@@ -81,9 +81,10 @@ Route::group(['middleware' => ['auth.shopify', 'shop-active']], function () {
     });
 
     Route::get('test', 'CustomerController@test');
-    Route::get('test', 'CustomerController@test');
+//    Route::get('test', 'CustomerController@test');
     Route::get('/webhook', 'UserController@webhooks');
     Route::get('/webhook/update', 'UserController@webhooks_update');
+
 
 
 });
